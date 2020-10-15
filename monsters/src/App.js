1ff8b9604da82d223562da62db1691600cc8551f
ravi19999 <dahalravindra@gmail.com>
2020-10-15 17:36:05 +0545

    Fetched data from the jsonplaceholder API instead of hardcoding it :recycle:

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstien",
-          id: "asss",
-        },
-        {
-          name: "Dracula",
-          id: "Aokks",
-        },
-        {
-          name: "Zombie",
-          id: "Hllsjd",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => this.setState({ monsters: users }))
+    );
   }
   render() {
     return (
