f6352f1c52f20b35d2413091648669216d219e28
ravi19999 <dahalravindra@gmail.com>
2020-10-15 20:06:48 +0545

    Fetched images from robohash.org and rendered images :lipstick:

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
-
 import "./App.css";
 
 class App extends Component {
@@ -21,11 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Ravi">
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
