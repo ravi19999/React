b15ac866c5b4da1d211d2ffa3935c1b9fde51541
ravi19999 <dahalravindra@gmail.com>
2020-10-16 23:32:21 +0545

    Updates

---
+++ precache-manifest.fed56ad4432fe630854667467e7cde05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813a9f12f97417621383457583d97be0",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "40293d92fc5761bd1a0e",
+    "url": "/React/static/css/main.9ee5a309.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/React/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40293d92fc5761bd1a0e",
+    "url": "/React/static/js/main.be71a6e9.chunk.js"
+  },
+  {
+    "revision": "87414ae963bd8b87f757",
+    "url": "/React/static/js/runtime-main.54d9d550.js"
+  }
+]);
